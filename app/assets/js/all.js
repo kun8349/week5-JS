cf234c9db7c698600261b74a36995a7faceb023f
kun8349 <108168475+kun8349@users.noreply.github.com>
2022-11-17 15:35:17 +0800

    11/17update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,6 @@
 let arr = [];
 const totalObj = {};
-const newData = [];
+let newData = [];
 
 axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
   .then(function (response){
@@ -13,19 +13,21 @@ axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelAp
     // step 1:totalObj 會變成 {高雄: 1, 台北: 1, 台中: 1}
     picListStep1();
     // step 2:newData = [["高雄", 1], ["台北",1], ["台中", 1]]
-    picListStep2();
+    newData = Object.entries(totalObj);
     // step 3:將 newData 丟入 c3 產生器
     picListStep3();
-    
+    console.log(newData);
+  })
+  .catch(function(err){
+    alert(err);
   })
 
 const list = document.querySelector('.ticketCard-area');
-// console.log(test);
 const searchNum = document.querySelector('.num');
 //預設執行函式
 function init(data){
   let str = '';
-  arr.forEach(function(i){
+  data.forEach(function(i){
     let test = `<li class="ticketCard">
   <div class="ticketCard-img">
     <a href="#">
@@ -68,51 +70,65 @@ const travelFilter = document.querySelector('.regionSearch');
 function travelEvent(){
   travelFilter.addEventListener('change',function(e){
     e.preventDefault();
-    let str = '';
-    arr.forEach(function(i){
-      let test = `<li class="ticketCard">
-      <div class="ticketCard-img">
-        <a href="#">
-          <img src=${i.imgUrl} alt="">
-        </a>
-        <div class="ticketCard-region">${i.area}</div>
-        <div class="ticketCard-rank">10</div>
-      </div>
-      <div class="ticketCard-content">
-        <div>
-          <h3>
-            <a href="#" class="ticketCard-name">${i.name}</a>
-          </h3>
-          <p class="ticketCard-description">
-            ${i.description}
-          </p>
-        </div>
-        <div class="ticketCard-info">
-          <p class="ticketCard-num">
-            <span><i class="fas fa-exclamation-circle"></i></span>
-            剩下最後 <span id="ticketCard-num"> ${i.group} </span> 組
-          </p>
-          <p class="ticketCard-price">
-            TWD <span id="ticketCard-price">${i.price}</span>
-          </p>
-        </div>
-      </div>
-                  </li>`;
-      if(e.target.value == '全部地區'){
-        str += test;
-        init(arr);
-        return;
-      }else if(e.target.value == i.area){
-        str += test;
-        let tempData = [];
-        if(i.area === e.target.value){
+    const val = e.target.value; //點選取 台北 台中 高雄的value
+    if(val === '全部地區'){
+      init(arr);
+      return;
+    }else{
+      let tempData = [];
+      arr.forEach((i)=>{
+        if(i.area === val){
           tempData.push(i);
-          console.log(tempData);
         }
-        searchNum.textContent = `本次搜尋共${tempData.length}筆資料`;
-      }
-    })
-    list.innerHTML = str; 
+      })
+      init(tempData);
+    }
+    
+    // let str = '';
+    // arr.forEach(function (i) {
+    //   let test = `<li class="ticketCard">
+    //   <div class="ticketCard-img">
+    //     <a href="#">
+    //       <img src=${i.imgUrl} alt="">
+    //     </a>
+    //     <div class="ticketCard-region">${i.area}</div>
+    //     <div class="ticketCard-rank">10</div>
+    //   </div>
+    //   <div class="ticketCard-content">
+    //     <div>
+    //       <h3>
+    //         <a href="#" class="ticketCard-name">${i.name}</a>
+    //       </h3>
+    //       <p class="ticketCard-description">
+    //         ${i.description}
+    //       </p>
+    //     </div>
+    //     <div class="ticketCard-info">
+    //       <p class="ticketCard-num">
+    //         <span><i class="fas fa-exclamation-circle"></i></span>
+    //         剩下最後 <span id="ticketCard-num"> ${i.group} </span> 組
+    //       </p>
+    //       <p class="ticketCard-price">
+    //         TWD <span id="ticketCard-price">${i.price}</span>
+    //       </p>
+    //     </div>
+    //   </div>
+    //               </li>`;
+    //   if (e.target.value == '全部地區') {
+    //     str += test;
+    //     init(arr);
+    //     return;
+    //   } else if (e.target.value == i.area) {
+    //     str += test;
+    //     let tempData = [];
+    //     if (i.area === e.target.value) {
+    //       tempData.push(i);
+    //       console.log(tempData);
+    //     }
+    //     searchNum.textContent = `本次搜尋共${tempData.length}筆資料`;
+    //   }
+    // })
+    // list.innerHTML = str; 
   })
 }
 
@@ -140,7 +156,9 @@ function travelAdd(){
     obj.description = addTicketDescription.value;
     arr.push(obj);
     init(arr);
+    
     clear();
+    
     let ary = Object.keys(totalObj);
     ary.forEach((i,x)=>{
       if(i === addTicketRegion.value){
@@ -148,7 +166,7 @@ function travelAdd(){
       }
     })
     //物件 
-    picListStep2();
+    newData = Object.entries(totalObj);
     picListStep3();
     document.querySelector(".addTicket-form").reset();
   })
@@ -167,7 +185,7 @@ function picListStep1(){
 
 
 
-
+//可用Object.entries()替代
 function picListStep2(){
   const areaAry = Object.keys(totalObj);
     areaAry.forEach((item)=>{
@@ -182,17 +200,30 @@ function picListStep2(){
 function picListStep3(){
   const chart = c3.generate({
     bindto: "#chart",
+
     data: {
       columns: newData,
       type : 'donut',
+      //修改圖表顏色,包在data裡面
+      colors: {
+        台北: "#6A6AFF",
+        台中: "#2894FF",
+        高雄: "#00FFFF"
+      },
     },
     donut: {
-      title: "地區"
+      title: "地區",
+      //修改圖表粗細,包在donut裡面
+      width: 15,
+      label:{
+        show:false,
+      }
     },
+    //修改圖表大小
     size: {
       height: 200,
       width: 200
-    }
+    },
   });
 }
 function clear(){
